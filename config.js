ecc8cd1ee12e1e4efaec006c0a9847e7253515e1
Nate Potter <ntpttr@gmail.com>
2018-06-29 14:44:02 -0700

    Change name to Covey

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-        development: "mongodb://localhost:27017/game-groups-dev",
-        test: "mongodb://localhost:27017/game-groups-test"
+        development: "mongodb://localhost:27017/covey-dev",
+        test: "mongodb://localhost:27017/covey-test"
     }
 };
