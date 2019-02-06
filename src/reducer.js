4937433d0d70833f7855bf4e19124b0912b2213e
Nate Potter <ntpttr@gmail.com>
2019-02-05 19:20:49 -0800

    Begin UI Refactor

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,9 @@
-import auth from './reducers/auth';
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import auth from './reducers/auth';
 import common from './reducers/common';
 import home from './reducers/home';
-import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
   auth,
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,34 +1,32 @@
-import {
-  LOGIN,
-  REGISTER,
-  LOGIN_PAGE_UNLOADED,
-  REGISTER_PAGE_UNLOADED,
-  ASYNC_START,
-  UPDATE_FIELD_AUTH
-} from '../constants/actionTypes';
+import {authConstants} from '../constants';
 
-export default (state = {}, action) => {
+let user = JSON.parse(localStorage.getItem('user'));
+const initialState = user ? { loggedIn: true, user } : {};
+
+export default (state = initialState, action) => {
   switch (action.type) {
-    case LOGIN:
-    case REGISTER:
+    case authConstants.LOGIN_REQUEST:
+    case authConstants.REGISTER_REQUEST:
       return {
-        ...state,
-        inProgress: false,
-        errors: action.error ? action.payload.errors : null
+        loggingIn: true,
+        user: action.user
       };
+    case authConstants.LOGIN_SUCCESS:
+    case authConstants.REGISTER_SUCCESS:
+      return {
+        loggedIn: true,
+        user: action.user
+      };
+    case authConstants.LOGIN_FAILURE:
+    case authConstants.REGISTER_FAILURE:
+    case authConstants.LOGOUT:
+      return {};
     case LOGIN_PAGE_UNLOADED:
     case REGISTER_PAGE_UNLOADED:
       return {};
-    case ASYNC_START:
-      if (action.subtype === LOGIN || action.subtype === REGISTER) {
-        return { ...state, inProgress: true };
-      }
-      break;
     case UPDATE_FIELD_AUTH:
       return { ...state, [action.key]: action.value };
     default:
       return state;
   }
-
-  return state;
 };
