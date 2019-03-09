fb4848762d90c5ca45e6b1c48c241beda8cb9606
Nate Potter <ntpttr@outlook.com>
2019-03-08 21:32:37 -0800

    Put back handleResponse

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -98,7 +98,7 @@ function logout() {
 function handleResponse(response) {
     return response.text().then(text => {
         const data = text && JSON.parse(text);
-        /*
+        
         if (!response.ok) {
             if (response.status === 401) {
                 // auto logout if 401 response returned from api
@@ -109,7 +109,7 @@ function handleResponse(response) {
             const error = (data && data.message) || response.statusText;
             return Promise.reject(error);
         }
-        */
+        
 
         return data;
     });
