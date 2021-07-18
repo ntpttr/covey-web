789a7a044dedf1ab790060704bd750bfc7f20c4c
Nate Potter <ntpttr@outlook.com>
2021-07-18 15:02:42 -0700

    Fix deprecated reload

--- src/helpers/responseHandler.js
+++ src/helpers/responseHandler.js
@@ -8,7 +8,7 @@ export function handleResponse(response) {
             if (response.status === 401) {
                 // auto logout if 401 response returned from api
                 logout();
-                window.location.reload(true);
+                window.location.reload();
             }
 
             const error = (data && data.message) || response.statusText;
